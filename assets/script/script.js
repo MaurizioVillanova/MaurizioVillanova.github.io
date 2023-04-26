f396c780ee1fe2c2b4af1c3f44de1bc287ebe0df
Maurizio Villanova <113591360+MaurizioVillanova@users.noreply.github.com>
2023-04-26 21:05:32 +0200

    pronto

---
+++ assets/script/script.js
@@ -0,0 +1,21 @@
+let textarea = $(".term");
+let text = "./villanova.mv";
+let i = 0;
+
+function runner() {
+    $(".container").hide();
+    
+    setTimeout(function () {
+        if (i < text.length) {
+            textarea.append(text.charAt(i));
+            i++;
+            runner();
+        } else {
+            i = 0;
+            $(".load").fadeOut(1000);
+            $(".container").fadeIn(1200);
+        }
+    }, 300);
+}
+
+runner();
